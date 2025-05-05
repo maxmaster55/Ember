33754dade9727a3ba17468a0d2adbae2c14743ff
maxmaster <yousseflkashef000@gmail.com>
2025-05-05 20:46:13 +0300

    formated code

--- src/parser.rs
+++ src/parser.rs
@@ -70,11 +70,10 @@ impl Parser {
         return stmnt;
     }
 
-
     fn parse_let_statement(&mut self) -> Result<Statement, String> {
         // Move to the identifier
         self.next_token();
-    
+
         // Expect identifier
         let identifier = match &self.current_token.t {
             TokenType::IDENT => self.current_token.literal.clone(),
@@ -85,29 +84,27 @@ impl Parser {
                 ));
             }
         };
-    
+
         // Expect '='
         self.next_token();
         if self.current_token.t != TokenType::ASSIGN {
-            return Err(format!(
-                "Expected '=', found {:?}",
-                self.current_token
-            ));
+            return Err(format!("Expected '=', found {:?}", self.current_token));
         }
-    
+
         // Move to the start of the expression
         self.next_token();
-    
+
         // Parse the expression on the right-hand side of the assignment
         let value = self.parse_expression()?;
-    
+
         // Construct the let statement
-        Ok(Statement::Let( LetStatement{name: identifier,value} ))
+        Ok(Statement::Let(LetStatement {
+            name: identifier,
+            value,
+        }))
     }
 
-
     fn parse_function_expression(&mut self) -> Result<Expression, String> {
-        
         // skip the function token
         self.next_token();
 
@@ -124,10 +121,7 @@ impl Parser {
         self.next_token();
 
         if self.current_token.t != TokenType::LPAREN {
-            return Err(format!(
-                "Expected '(', found {:?}",
-                self.current_token
-            ));
+            return Err(format!("Expected '(', found {:?}", self.current_token));
         }
 
         self.next_token(); // Skip the '('
@@ -156,24 +150,16 @@ impl Parser {
         }
 
         if self.current_token.t != TokenType::RPAREN {
-            return Err(format!(
-                "Expected ')', found {:?}",
-                self.current_token
-            ));
-            
+            return Err(format!("Expected ')', found {:?}", self.current_token));
         }
 
         self.next_token(); // Skip the ')'
 
         if self.current_token.t != TokenType::LBRACE {
-            return Err(format!(
-                "Expected '{{', found {:?}",
-                self.current_token
-            ));
+            return Err(format!("Expected '{{', found {:?}", self.current_token));
         }
 
         self.next_token(); // Skip the '{'
-
 
         let mut body = vec![];
 
@@ -185,15 +171,11 @@ impl Parser {
             }
             let stmnt = self.parse_statement()?;
             body.push(stmnt);
-            
-        };
+        }
 
         // Check for the closing brace
         if self.current_token.t != TokenType::RBRACE {
-            return Err(format!(
-                "Expected '}}', found {:?}",
-                self.current_token
-            ));
+            return Err(format!("Expected '}}', found {:?}", self.current_token));
         }
 
         self.next_token(); // Skip the '}'
@@ -203,12 +185,11 @@ impl Parser {
         let ex = Expression::FUNCTION {
             name,
             parameters,
-            body: body
+            body: body,
         };
 
         return Ok(ex);
     }
-
 
     fn parse_return_statement(&mut self) -> Result<Statement, String> {
         self.next_token(); // Skip the "return" token
@@ -245,10 +226,9 @@ impl Parser {
 
     fn parse_if_expression(&mut self) -> Result<Expression, String> {
         self.next_token(); // Skip the "if" token
-        
+
         // Parse the condition
         let cond = self.parse_condition()?;
-
 
         if self.current_token.t != TokenType::LBRACE {
             return Err("Expected '{' after an IF condition".to_string());
@@ -321,7 +301,6 @@ impl Parser {
     fn parse_expression(&mut self) -> Result<Expression, String> {
         let mut left = self.parse_primary_expression()?;
 
-
         while Parser::is_operator(&self.peek_token) {
             left = self.parse_infix_expression(left)?;
         }
@@ -329,33 +308,30 @@ impl Parser {
         if self.peek_token.t == TokenType::RPAREN {
             self.next_token();
         }
-        
+
         Ok(left)
     }
 
     fn parse_infix_expression(&mut self, left: Expression) -> Result<Expression, String> {
-        
         self.next_token();
         // Check if the next is a )
 
         let operator = self.current_token.literal.clone();
 
         self.next_token();
-    
+
         let right = self
             .parse_expression()
             .map_err(|err| format!("Error parsing right-hand side of infix expression: {}", err))?;
-    
+
         Ok(Expression::INFEX {
             left: Box::new(left),
             operator,
             right: Box::new(right),
         })
     }
-    
+
     fn parse_primary_expression(&mut self) -> Result<Expression, String> {
-
-
         match self.current_token.t {
             TokenType::IF => self.parse_if_expression(),
             TokenType::FUNCTION => self.parse_function_expression(),
@@ -373,15 +349,15 @@ impl Parser {
 
     fn parse_grouped_expression(&mut self) -> Result<Expression, String> {
         self.next_token(); // Skip '('
-    
+
         let expr = self.parse_expression()?; // Parse full expression like 5 + 5
-    
+
         // Now advance once to move to the next token, which should be ')'
         self.next_token();
-        
+
         Ok(expr)
     }
-    
+
     fn parse_integer_literal(&mut self) -> Result<Expression, String> {
         let value = self
             .current_token
@@ -404,16 +380,16 @@ impl Parser {
     }
 
     fn parse_identifier_expression(&mut self) -> Result<Expression, String> {
-
         if self.peek_token.t == TokenType::SEMICOLON {
             return Ok(Expression::IDENT(self.current_token.literal.clone()));
-        }else if Parser::is_operator(&self.peek_token) {
-            return self.parse_infix_expression(Expression::IDENT(self.current_token.literal.clone()))
+        } else if Parser::is_operator(&self.peek_token) {
+            return self
+                .parse_infix_expression(Expression::IDENT(self.current_token.literal.clone()));
         } else {
             return Err(format!(
                 "Unexpected token {:?} in identifier expression",
                 self.current_token
-            ))
+            ));
         }
     }
 
@@ -434,8 +410,6 @@ impl Parser {
         })
     }
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -494,7 +468,12 @@ mod tests {
         assert_eq!(program.statements.len(), 1);
 
         if let Statement::Expression(expr_stmt) = &program.statements[0] {
-            if let Expression::IF { condition, consequence, alternative } = &expr_stmt.expression {
+            if let Expression::IF {
+                condition,
+                consequence,
+                alternative,
+            } = &expr_stmt.expression
+            {
                 if let Expression::INFEX { ref operator, .. } = **condition {
                     assert_eq!(operator, "<");
                 } else {
@@ -595,7 +574,12 @@ mod tests {
         assert_eq!(program.statements.len(), 1);
 
         if let Statement::Expression(expr_stmt) = &program.statements[0] {
-            if let Expression::FUNCTION { name, parameters, body } = &expr_stmt.expression {
+            if let Expression::FUNCTION {
+                name,
+                parameters,
+                body,
+            } = &expr_stmt.expression
+            {
                 assert_eq!(name, "myFunc");
                 assert_eq!(parameters.len(), 2);
                 assert_eq!(parameters[0], "x");
@@ -627,5 +611,4 @@ mod tests {
             panic!("Expected expression statement");
         }
     }
-
 }
